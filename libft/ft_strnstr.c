9c89d7cea3e7d4b01a9e7fb6f3a5f5b99a1364ec
Noe Morgado Moreno <nmorgado@c2r17s1.42madrid.com>
2024-09-27 17:12:34 +0200

    Finalizado strnstr

--- libft/ft_strnstr.c
+++ libft/ft_strnstr.c
@@ -6,47 +6,51 @@
 /*   By: nmorgado <nmorgado@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/26 17:54:24 by nmorgado          #+#    #+#             */
-/*   Updated: 2024/09/26 18:27:10 by nmorgado         ###   ########.fr       */
+/*   Updated: 2024/09/27 17:10:12 by nmorgado         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stddef.h>
 
-char	*ft_strnstr(const char *big, const char *little, size_t len)
+char	*ft_strnstr2(char *point, const char *big, const char *lit, size_t len)
 {
 	size_t	i;
 	size_t	j;
-	char	*pointer;
 
-	j = 0;
-	i = 0;
-	if (ft_strlen(little) == 0)
-	{
-		pointer = (char *) big;
-		return (pointer);
-	}
-	
-	j = 0;
 	i = 0;
 	while (i < len)
 	{
-		if (big[i] == little[j])
+		j = 0;
+		if (big[i] == lit[j])
 		{
-			pointer = (char *) &big[i];
-			while (big[i] == little[j] && j < ft_strlen(little) && i < len)
+			point = (char *) &big[i];
+			while (big[i] == lit[j] && j < ft_strlen(lit) && i < len)
 			{
 				i++;
 				j++;
 			}
-			if (j == ft_strlen(little))
-				return (pointer);
+			if (j == ft_strlen(lit))
+				return (point);
 			else
 			{
-				j = 0;
-				pointer = 0;
+				point = 0;
 			}
 		}
 		i++;
 	}
 	return (0);
+}
+
+char	*ft_strnstr(const char *big, const char *little, size_t len)
+{
+	char	*pointer;
+
+	if (ft_strlen(little) == 0)
+	{
+		pointer = (char *) big;
+		return (pointer);
+	}
+	else
+		pointer = 0;
+	return (ft_strnstr2(pointer, big, little, len));
 }
