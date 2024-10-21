f001c6e17ee01b2348a1f5a254f2fd9f09394695
Noé <morgadomorenonoe@gmail.com>
2024-10-21 18:31:56 +0200

    correccion split

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -3,14 +3,53 @@
 /*                                                        :::      ::::::::   */
 /*   ft_split.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nmorgado <nmorgado@student.42.fr>          +#+  +:+       +#+        */
+/*   By: nmorgado <nmorgado@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/18 17:26:57 by nmorgado          #+#    #+#             */
-/*   Updated: 2024/10/18 17:27:03 by nmorgado         ###   ########.fr       */
+/*   Updated: 2024/10/21 18:30:37 by nmorgado         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+static	void	free_split(char **result)
+{
+	int	i;
+
+	i = -1;
+	while (result[i])
+	{
+		free(result[i++]);
+	}
+	free(result);
+}
+
+static	int	make_string(const char *s, char c, char **result)
+{
+	int			i;
+	const char	*start;
+
+	i = 0;
+	while (*s)
+	{
+		if (*s != c)
+		{
+			start = s;
+			while (*s && *s != c)
+				s++;
+			result[i] = ft_substr(start, 0, s - start);
+			if (!result[i])
+			{
+				free_split(result);
+				return (-1);
+			}
+			i++;
+		}
+		else
+			s++;
+	}
+	return (i);
+}
 
 static int	ft_count_words(const char *s, char c)
 {
@@ -38,26 +77,14 @@ char	**ft_split(const char *s, char c)
 	char		**result;
 	int			i;
 	int			wc;
-	const char	*start;
 
 	wc = ft_count_words(s, c);
 	result = malloc((wc + 1) * sizeof(char *));
 	if (!result)
 		return (NULL);
-	i = 0;
-	while (*s)
-	{
-		if (*s != c)
-		{
-			start = s;
-			while (*s && *s != c)
-				s++;
-			result[i] = ft_substr(start, 0, s - start);
-			i++;
-		}
-		else
-			s++;
-	}
+	i = make_string(s, c, result);
+	if (i == -1)
+		return (NULL);
 	result[i] = NULL;
 	return (result);
 }
