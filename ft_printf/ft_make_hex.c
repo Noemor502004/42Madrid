73b7b84d3a82a846180e88e68d8d7041e91cb1ba
Noé <morgadomorenonoe@gmail.com>
2024-12-16 10:53:22 +0100

    Solo queda normi de unsigned, totalmente funcional

--- ft_printf/ft_make_hex.c
+++ ft_printf/ft_make_hex.c
@@ -6,23 +6,41 @@
 /*   By: nmorgado <nmorgado@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/16 09:05:02 by nmorgado          #+#    #+#             */
-/*   Updated: 2024/12/16 09:05:31 by nmorgado         ###   ########.fr       */
+/*   Updated: 2024/12/16 10:51:44 by nmorgado         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_make_hex(unsigned long integer, int cap_bool)
+void	make_last(char *str_int, unsigned long integer, int cap_bool,
+char *string2)
 {
-	char	*string2;
-	char	*str_int;
-	int		i;
-	int		rest;
+	if (integer != 0)
+	{
+		if (integer >= 10)
+		{
+			if (cap_bool)
+				integer += 55;
+			else
+				integer += 87;
+			str_int[ft_strlen(str_int)] = integer;
+		}
+		else
+		{
+			string2 = ft_itoa(integer);
+			str_int[ft_strlen(str_int)] = *string2;
+			free (string2);
+		}
+	}
+}
+
+int	make_string(char *str_int, unsigned long integer, int cap_bool,
+char *string2)
+{
+	int	i;
+	int	rest;
 
 	i = -1;
-	str_int = ft_calloc(17, sizeof(char));
-	if (!str_int)
-		return (0);
 	while (integer >= 16)
 	{
 		rest = integer % 16;
@@ -42,23 +60,20 @@ char	*ft_make_hex(unsigned long integer, int cap_bool)
 			free (string2);
 		}
 	}
-	if (integer != 0)
-	{
-		if (integer >= 10)
-		{
-			if (cap_bool)
-				integer += 55;
-			else
-				integer += 87;
-			str_int[++i] = integer;
-		}
-		else
-		{
-			string2 = ft_itoa(integer);
-			str_int[++i] = *string2;
-			free (string2);
-		}
-	}
+	return (integer);
+}
+
+char	*ft_make_hex(unsigned long integer, int cap_bool)
+{
+	char	*string2;
+	char	*str_int;
+
+	str_int = ft_calloc(17, sizeof(char));
+	string2 = 0;
+	if (!str_int)
+		return (0);
+	integer = make_string(str_int, integer, cap_bool, string2);
+	make_last(str_int, integer, cap_bool, string2);
 	str_int = ft_str_reverse(str_int);
 	return (str_int);
 }
