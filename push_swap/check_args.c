5c69eb26d1fe20bc3c109f25dc16949f1a0af335
Noe Morgado Moreno <nmorgado@c2r16s1.42madrid.com>
2025-01-29 18:20:51 +0100

    Cositas

--- push_swap/check_args.c
+++ push_swap/check_args.c
@@ -6,16 +6,17 @@
 /*   By: nmorgado <nmorgado@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/27 17:53:30 by nmorgado          #+#    #+#             */
-/*   Updated: 2025/01/27 17:59:27 by nmorgado         ###   ########.fr       */
+/*   Updated: 2025/01/28 16:47:22 by nmorgado         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 void	check_int(char *argv[], int i)
 {
 	int	j;
+	int	numb;
 
-	if (ft_strlen(*argv[i]) > 10
-		|| (ft_strlen(*argv[i]) == 10 && *argv[9] > 55))
+	numb = ft_atoi(*argv[i]);
+	if (numb > 2147483647 || numb < -2147483648)
 	{
 		write(1, "Error\n", 6);
 		_exit(-1);
--- push_swap/push_swap.c
+++ push_swap/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: nmorgado <nmorgado@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/27 16:53:55 by nmorgado          #+#    #+#             */
-/*   Updated: 2025/01/27 17:59:17 by nmorgado         ###   ########.fr       */
+/*   Updated: 2025/01/28 16:56:27 by nmorgado         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,20 +19,30 @@ void	push_swap(int number, ...)
 
 int	main(int argc, char *argv[])
 {
-	int	*array;
+	int	*a;
 	int	i;
 	int	j;
 
 	j = 0;
 	i = 1;
-	array = (int *)ft_calloc(1, sizeof(int));
+	a = (int *)ft_calloc(ft_strlen(argv), sizeof(int));
 	if (argc < 2)
 		_exit(0);
 	while (*argv[i] != '\0')
 	{
 		check_int(argv, i);
-		check_duplicate(array, argv, i);
-		array[j - 1] = ft_atoi(*argv[i]);
+		check_duplicate(a, argv, i);
+		a[j - 1] = ft_atoi(*argv[i]);
 		i++;
 	}
 }
+/*
+1
+6
+8
+3
+5
+9
+
+
+*/
